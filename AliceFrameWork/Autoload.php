09781152ed1c611472362bfa293ae9891676e89d
leunico <867426952@qq.com>
2016-03-17 10:18:22 +0800

    Update Autoload.php

--- AliceFrameWork/Autoload.php
+++ AliceFrameWork/Autoload.php
@@ -8,22 +8,22 @@
 namespace AliceFrameWork;
 
 class Autoload{
-
-    public function register(){
+	
+	public function register(){
 	
 		spl_autoload_register(array($this,'autoload'));
 			
 	}
-		
+	
 	public function autoload($className){
 	
 		$pathArr = explode('\\',$className);
-        
+	        
 		$filename = array_pop($pathArr);
         
 		$dir = implode(DIRECTORY_SEPARATOR,$pathArr);
         
-        $filename = $dir.'/'.$filename.'.php'; //var_dump($filename.'--==--'.$className.'</br>');
+        	$filename = $dir.'/'.$filename.'.php'; //var_dump($filename.'--==--'.$className.'</br>');
         
 		if(file_exists($filename)){	
             
@@ -33,9 +33,10 @@ class Autoload{
             
 			#exit('Error:'.$className.' loading Failed'); // 调试模式
 			#echo "Error:".$className." loading Failed <br/>"; // 兼容模式
-            NotFound(); //运营
+			NotFound(); //运营
             
 		}
 				
-	}	
+	}
+	
 }
