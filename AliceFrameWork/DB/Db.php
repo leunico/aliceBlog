9165cc1edc0fa9f5e95510fa50e529c22c832bfa
leunico <867426952@qq.com>
2016-03-30 13:54:34 +0800

    Update Db.php

--- AliceFrameWork/DB/Db.php
+++ AliceFrameWork/DB/Db.php
@@ -1,31 +1,22 @@
-<?php
-
+<?php namespace AliceFrameWork\DB;
 /*
  * 数据库扩展脚本
  * @leunico
  */
 
-namespace AliceFrameWork\DB;
-
-class Db{
-
-   public static function factor(){
-	     
-	  $dbType = strtolower(DB_TYPE);	  
-	  switch($dbType){
-		  
-		  case 'mysql':		  
-		     $className = 'Mysql';			 
-			 break;			 
-		  default:		  
-		     exit('Error: Database Type');  
-		  
-	  }
-	  
-	  $className = 'AliceFrameWork\DB\\'.$className;	  
-	  return new $className();
-	   
-   }
-	
-	
+class Db
+{
+    public static function factor()
+    {
+        $dbType = strtolower(DB_TYPE);
+        switch ($dbType) {
+            case 'mysql':
+                $className = 'Mysql';
+                break;
+            default:
+                die('Error: Database Type');
+        }
+        $className = 'AliceFrameWork\\DB\\' . $className;
+        return new $className();
+    }
 }
