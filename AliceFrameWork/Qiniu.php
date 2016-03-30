57bcc1c71bbdb3a4ef673b45200c6cf6b68296e1
leunico <867426952@qq.com>
2016-03-30 11:58:36 +0800

    Update Qiniu.php

--- AliceFrameWork/Qiniu.php
+++ AliceFrameWork/Qiniu.php
@@ -1,36 +1,48 @@
-<?php
-
+<?php namespace AliceFrameWork;
 /*  
  *  七牛云操作文件
  *  @leunico 
  */
- 
-namespace AliceFrameWork;
 
-require_once ADMIN_QINIU_DIR.'autoload.php';
+require_once ADMIN_QINIU_DIR . 'autoload.php';
 
 use Qiniu\Auth;
 use Qiniu\Storage\BucketManager;
 use Qiniu\Storage\UploadManager;
 
-class Qiniu {
-	
-	private $accessKey = 'BfR2VxdEwZavgGM3kVChc6F0NKwGMTPMUdUsywtz';
+class Qiniu
+{
+    
+    private $accessKey = 'BfR2VxdEwZavgGM3kVChc6F0NKwGMTPMUdUsywtz';
     private $secretKey = 'jhifcIoBhyH_AQBsxAHChXaR9nWMed3F07pOI4JM';
-	private $bucket = "";
-	private $Auth = "";
-	public function __construct($bucket = ''){	
-	
-	     $this->Auth = new Auth($this->accessKey, $this->secretKey);
-		 $this->bucket = $bucket ? $bucket : "lzxya";
-		 
-	}
-	
-	public function Delete($filename){
-		
-		 $Bucket = new BucketManager($this->Auth);
-		 $ret = $Bucket->delete($this->bucket,$filename);
-
-	}
-	
+    private $bucket;
+    private $Auth;
+    public function __construct($bucket = '')
+    {
+        
+        $this->auth = new Auth($this->accessKey, $this->secretKey);
+        $this->bucket = $bucket ? $bucket : "lzxya";
+        
+    }
+    
+    public function Delete($filename)
+    {
+        
+        $Bucket = new BucketManager($this->auth);
+        $err = $Bucket->delete($this->bucket, $filename);
+        return ($err !== null) ? '' : true;
+        
+    }
+    
+    public function PutImgFile($filename, $filePath, $category)
+    {
+        
+        // 生成上传Token
+        $token = $this->auth->uploadToken($this->bucket);
+        $uploadMgr = new UploadManager();
+        list($ret, $err) = $uploadMgr->putFile($token, $filename, $filePath);
+        return ($err !== null) ? '' : QINIU_DIR_THUMB . $ret['key'] . $category;
+        
+    }
+    
 }
