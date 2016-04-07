6ebf07fdf27cd5a530e73f944b618a0929c02a87
leunico <867426952@qq.com>
2016-04-07 16:16:07 +0800

    Update classController.php

--- app/Controller/classController.php
+++ app/Controller/classController.php
@@ -1,153 +1,150 @@
-<?php 
-
-namespace app\Controller;
+<?php namespace app\Controller;
 
 use AliceFrameWork\View;
 use AliceFrameWork\Request;
-use app\Model\ArticleModel;
-use app\Model\DiaryModel;
-use AliceFrameWork\Memcache;
 
-class classController{
-    	
-	public function index(){
-        
-        View::AdminErrorMessage('goback',"非法入口,邪灵退散！！！");
-		
-	}
+class classController extends Controller
+{
+    public function index()
+    {
+        View::AdminErrorMessage('goback', '非法入口,邪灵退散！！！');
+    }
     
-	private static function ClassData($class){
-        
+    private static function ClassData($class)
+    {
         $fields = Request::getRequest('page', 'int');
-        $page = isset($fields) && $fields > 0 ? $fields : 1;      
+        $page = isset($fields) && $fields > 0 ? $fields : 1;
+        $article = self::$models->Article;
+        $mem = self::$models->Memcache;
         $configclass = getClass();
         $meunclass = $configclass['menu_class'];
         $articleclass = $configclass['article_class'];
-        if(empty($meunclass[$class]) && empty($articleclass[$class])) View::AdminErrorMessage('goback','入口错误误误！');   
-        $mem = new Memcache();    
-        $ret = $mem->get('classController_ClassData_'.$class.$page);
+        if (empty($meunclass[$class]) && empty($articleclass[$class])) {
+            View::AdminErrorMessage('goback', '入口错误误误！');
+        }
+        $ret = $mem->get('classController_ClassData_' . $class . $page);
         $memc = $class;
-        if(!empty($meunclass[$class]) && is_array($meunclass[$class])){
-            $class = $meunclass[$class]; 
+        if (!empty($meunclass[$class]) && is_array($meunclass[$class])) {
+            $class = $meunclass[$class];
             array_shift($class);
             $class = array_flip($class);
-        }         
-        if(empty($ret)){
-			$ret = array();    	      
-            $ret['articleClassList'] = ArticleModel::getArticleClassList($class,$page);
+        }
+        if (empty($ret)) {
+            $ret = array();
+            $ret['articleClassList'] = $article->getArticleClassList($class, $page);
             $ret['pageNav'] = @array_pop($ret['articleClassList']);
-            $mem->set('classController_ClassData_'.$memc.$page,$ret);
+            $mem->set('classController_ClassData_' . $memc . $page, $ret);
         }
-		$ret['meunclass'] = $meunclass;
-		$ret['articleclass'] = $articleclass;
-		$ret['nav'] = $class;
-        View::Transmit('newclassshow',$ret);
-		
+        $ret['meunclass'] = $meunclass;
+        $ret['articleclass'] = $articleclass;
+        $ret['nav'] = $class;
+        View::Transmit('newclassshow', $ret);
     }
     
-    //>>>导航入口控制扩展    
-     public function timewait(){
-        
+    //>>>导航入口控制扩展
+    public function timewait()
+    {
         $ret = array();
-        $ret['timewait'] = DiaryModel::getTimewiatIndex();
-        View::Transmit('twait',$ret);
-         
+        $diary = self::$models->Diary;
+        $ret['timewait'] = $diary->getTimewiatIndex();
+        View::Transmit('twait', $ret);
     }
     
-    public function articlebox(){
-        
-        $mem = new Memcache();    
+    public function articlebox()
+    {
+        $mem = self::$models->Memcache;
+        $article = self::$models->Article;
         $ret = $mem->get('classController_articlebox');
-        if(empty($ret)){
-			$ret = array();    	      
-            $ret['articleClassList'] = ArticleModel::getArticleBox();
-            $mem->set('classController_articlebox',$ret,12*3600);
+        if (empty($ret)) {
+            $ret = array();
+            $ret['articleClassList'] = $article->getArticleBox();
+            $mem->set('classController_articlebox', $ret, 12 * 3600);
         }
-        View::Transmit('articlebox',$ret);
-        
+        View::Transmit('articlebox', $ret);
     }
     
-    public function meclass(){
-       
+    public function meclass()
+    {
         View::Transmit('aboutme');
-    } 
-    
-    public function me(){
-                
-        View::Transmit('aboutme');
-        
     }
     
-    public function liuy(){
-                
+    public function me()
+    {
+        View::Transmit('aboutme');
+    }
+    
+    public function liuy()
+    {
         View::Transmit('liuy');
-        
     }
     
-    public function php(){
-
-        self::ClassData('php');   
+    public function php()
+    {
+        self::ClassData('php');
     }
     
-    public function music(){
-
-        self::ClassData('music');   
+    public function music()
+    {
+        self::ClassData('music');
     }
     
-    public function js(){
-
-        self::ClassData('js');   
+    public function js()
+    {
+        self::ClassData('js');
     }
     
-    public function symfony(){
-
-        self::ClassData('symfony');   
+    public function backend()
+    {
+        self::ClassData('backend');
     }
     
-    public function laravel(){
-
-        self::ClassData('laravel');   
+    public function laravel()
+    {
+        self::ClassData('laravel');
     }
     
-    public function left(){
-
-        self::ClassData('left');   
+    public function left()
+    {
+        self::ClassData('left');
     }
     
-    public function linux(){
-
-        self::ClassData('linux');   
+    public function linux()
+    {
+        self::ClassData('linux');
     }
     
-    public function game(){
-
-        self::ClassData('game');   
+    public function game()
+    {
+        self::ClassData('game');
     }
     
-    public function book(){
-
-        self::ClassData('book');   
+    public function book()
+    {
+        self::ClassData('book');
     }
     
-    public function acbili(){
-
-        self::ClassData('acbili');   
+    public function acbili()
+    {
+        self::ClassData('acbili');
     }
     
-    public function jishu(){
-
-        self::ClassData('jishu');   
+    public function jishu()
+    {
+        self::ClassData('jishu');
+    }
+    
+    public function python()
+    {
+        self::ClassData('python');
     }
     
     /*public function kaiyuan(){
-
-        self::ClassData('kaiyuan');         
-    }*/
-    
-    public function like(){
-
-        self::ClassData('like');   
-    }    
-    
+      self::ClassData('kaiyuan');         
+      }*/
+      
+    public function like()
+    {
+        self::ClassData('like');
+    }
     
 }
