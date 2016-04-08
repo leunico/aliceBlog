91221193e0c38766b6bd6080dd19e30c392fa02e
leunico <867426952@qq.com>
2016-04-08 10:56:45 +0800

    Update AdminModel.php

--- app/Model/AdminModel.php
+++ app/Model/AdminModel.php
@@ -1,59 +1,56 @@
-<?php
-
-namespace app\Model;
+<?php namespace app\Model;
 
 use AliceFrameWork\Example;
-use AliceFrameWork\Memcache;
 use AliceFrameWork\Request;
 use AliceFrameWork\View;
 
-class AdminModel{
-		
-	public static function getByUser($email){
-	   
-		$ret = new Example('admin_user');
-		return $ret->Find('email',$email);
-	}
-	
-	public static function getByUserId($id){
-	   
-		$ret = new Example('admin_user');
-		return $ret->Find('id',$id);
-	}
-	
-	public static function getByArticleId($id){
-	   
-		$ret = new Example('info_article');
-		return $ret->Find('id',$id,'uid');
-	}
-	
-	public static function MemUpdata(){
-		
-		$lasttime = Request::getSession('MemacheData') ? Request::getSession('MemacheData'):1444895435;       
-        if(time() < ($lasttime + 5)){            
-            View::AdminErrorMessage('goback', '操作过于频繁');           
-        }else{                     
-            if(MEMCACHE == TRUE){
-                $mem = new Memcache();               
-                $mem->clear(); 
-            }else{
-                View::AdminErrorMessage('goback', '网站未开启缓存');	   
+class AdminModel
+{
+    private $example;
+    
+    public function __construct()
+    {
+        $this->example = new Example('admin_user');
+    }
+    
+    public function getByUser($email)
+    {
+        return $this->example->Find('email', $email);
+    }
+    
+    public function getByUserId($id)
+    {
+        return $this->example->Find('id', $id);
+    }
+    
+    public function getByArticleId($id)
+    {
+        return $this->example->setBind('info_article')->Find('id', $id, 'uid');
+    }
+    
+    public function MemUpdata($mem)
+    {
+        $lasttime = Request::getSession('MemacheData') ? Request::getSession('MemacheData') : 1444895435;
+        if (time() < $lasttime + 5) {
+            View::AdminErrorMessage('goback', '操作过于频繁');
+        } else {
+            if (MEMCACHE == TRUE) {
+                $mem->clear();
+            } else {
+                View::AdminErrorMessage('goback', '网站未开启缓存');
             }
             Request::setSession('MemacheData', time());
-       }
-       View::AdminMessage('goback', "缓存更新成功!"); 
-		
-	}
-	
-	public static function MemClear(){
-		
-	   if(MEMCACHE == TRUE){
-		   $mem = new Memcache();               
-		   $mem->clear(); 
-	   }else{
-		   return FALSE;
-	   }
-		
+        }
+        View::AdminMessage('goback', '缓存更新成功!');
     }
-	
+    
+    public function MemClear($mem)
+    {
+        if (MEMCACHE == TRUE) {
+            $mem->clear();
+        } else {
+            return FALSE;
+        }
+    }
+    
 }
