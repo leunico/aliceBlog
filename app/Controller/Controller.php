c2014e82b9563d8b99d58297a5f0034789167c2c
leunico <867426952@qq.com>
2016-04-08 12:02:12 +0800

    Update Controller.php

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -7,19 +7,19 @@ class Controller
     public function __construct($model)
     {
         //注册！
-        $model->Index = 'app\\Model\\IndexModel';
-        $model->Admin = 'app\\Model\\AdminModel';
-        $model->User = 'app\\Model\\UserModel';
-        $model->Article = 'app\\Model\\ArticleModel';
-        $model->Comment = 'app\\Model\\CommentModel';
-        $model->Diary = 'app\\Model\\DiaryModel';
-        $model->Tag = 'app\\Model\\TagModel';
-        $model->Memcache = 'AliceFrameWork\\Memcache';
-        $model->WeixinClass = 'AliceFrameWork\\weixin\\WeixinClass';
-        $model->Weixin = 'AliceFrameWork\\weixin\\Weixin';
-        $model->JinShan = 'AliceFrameWork\\JinShan';
-        $model->Qiniu = 'AliceFrameWork\\Qiniu';
-        $model->SmtpMail = 'AliceFrameWork\\SmtpMail';
+        $model->Index = 'app\Model\IndexModel';
+        $model->Admin = 'app\Model\AdminModel';
+        $model->User = 'app\Model\UserModel';
+        $model->Article = 'app\Model\ArticleModel';
+        $model->Comment = 'app\Model\CommentModel';
+        $model->Diary = 'app\Model\DiaryModel';
+        $model->Tag = 'app\Model\TagModel';
+        $model->Memcache = 'AliceFrameWork\Memcache';
+        $model->WeixinClass = 'AliceFrameWork\weixin\WeixinClass';
+        $model->Weixin = 'AliceFrameWork\weixin\Weixin';
+        $model->JinShan = 'AliceFrameWork\JinShan';
+        $model->Qiniu = 'AliceFrameWork\Qiniu';
+        $model->SmtpMail = 'AliceFrameWork\SmtpMail';
         self::$models = $model;
     }
     
