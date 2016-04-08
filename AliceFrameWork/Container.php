4091ea4c94467feb327ca35a18e3074e321c4f5d
leunico <867426952@qq.com>
2016-04-08 12:00:55 +0800

    Update Container.php

--- AliceFrameWork/Container.php
+++ AliceFrameWork/Container.php
@@ -1,106 +1,75 @@
-<?php
-
+<?php namespace AliceFrameWork;
 /*  
  *  模型容器文件
  *  @leunico 
  */
- 
-namespace AliceFrameWork;
 
 use ReflectionClass;
 
-class Container{
-
+class Container
+{
     private $app = array();
     
     private $instances = array();
-
-    public function __set($key, $concrete){
-
+    
+    public function __set($key, $concrete)
+    {
         $this->app[$key] = $concrete;
     }
-
-    public function __get($key){
-
+    
+    public function __get($key)
+    {
         return $this->build($this->app[$key]);
     }
     
-    public function make($abstract,$parameters){
-        
+    public function make($abstract, $parameters)
+    {
         $this->instances = $parameters;
-        
         return $this->build($this->app[$abstract]);
     }
-
-    public function build($className){
-
+    
+    public function build($className)
+    {
         if ($className instanceof Closure) {
-            
             return $className($this);
         }
-
         $reflector = new ReflectionClass($className);
-
         if (!$reflector->isInstantiable()) {
-            
-            throw new Exception("Error:Can't instantiate this.");
-            
+            throw new Exception('Error:Can\'t instantiate this.');
         }
-
         $constructor = $reflector->getConstructor();
-
         if (is_null($constructor)) {
-            
-            return new $className;
-            
+            return new $className();
         }
-        
-        if($this->instances){
-
+        if ($this->instances) {
             return $reflector->newInstanceArgs($this->instances);
-
-        }else{
-            
+        } else {
             $parameters = $constructor->getParameters();
-
             $dependencies = $this->getDependencies($parameters);
-
             return $reflector->newInstanceArgs($dependencies);
         }
-
     }
-
-    public function getDependencies($parameters){
-
-        $dependencies = [];
-
+    
+    public function getDependencies($parameters)
+    {
+        $dependencies = array();
         foreach ($parameters as $parameter) {
-
             $dependency = $parameter->getClass();
-
             if (is_null($dependency)) {
-
                 $dependencies[] = $this->resolveNonClass($parameter);
-                
             } else {
-
                 $dependencies[] = $this->build($dependency->name);
             }
         }
         return $dependencies;
-        
     }
-
-    public function resolveNonClass($parameter){
-
+    
+    public function resolveNonClass($parameter)
+    {
         if ($parameter->isDefaultValueAvailable()) {
-            
             return $parameter->getDefaultValue();
-            
         }
-        
         throw new Exception('Error:Missing parameter.');
-
     }
     
 }
