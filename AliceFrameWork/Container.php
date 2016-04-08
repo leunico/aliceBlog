9a4dbc1db0f87549a07ae4bf3e4c20ffbc7805b3
leunico <867426952@qq.com>
2016-04-08 11:58:21 +0800

    更新程序

---
+++ AliceFrameWork/Container.php
@@ -0,0 +1,106 @@
+<?php
+
+/*  
+ *  模型容器文件
+ *  @leunico 
+ */
+ 
+namespace AliceFrameWork;
+
+use ReflectionClass;
+
+class Container{
+
+    private $app = array();
+    
+    private $instances = array();
+
+    public function __set($key, $concrete){
+
+        $this->app[$key] = $concrete;
+    }
+
+    public function __get($key){
+
+        return $this->build($this->app[$key]);
+    }
+    
+    public function make($abstract,$parameters){
+        
+        $this->instances = $parameters;
+        
+        return $this->build($this->app[$abstract]);
+    }
+
+    public function build($className){
+
+        if ($className instanceof Closure) {
+            
+            return $className($this);
+        }
+
+        $reflector = new ReflectionClass($className);
+
+        if (!$reflector->isInstantiable()) {
+            
+            throw new Exception("Error:Can't instantiate this.");
+            
+        }
+
+        $constructor = $reflector->getConstructor();
+
+        if (is_null($constructor)) {
+            
+            return new $className;
+            
+        }
+        
+        if($this->instances){
+
+            return $reflector->newInstanceArgs($this->instances);
+
+        }else{
+            
+            $parameters = $constructor->getParameters();
+
+            $dependencies = $this->getDependencies($parameters);
+
+            return $reflector->newInstanceArgs($dependencies);
+        }
+
+    }
+
+    public function getDependencies($parameters){
+
+        $dependencies = [];
+
+        foreach ($parameters as $parameter) {
+
+            $dependency = $parameter->getClass();
+
+            if (is_null($dependency)) {
+
+                $dependencies[] = $this->resolveNonClass($parameter);
+                
+            } else {
+
+                $dependencies[] = $this->build($dependency->name);
+            }
+        }
+        return $dependencies;
+        
+    }
+
+    public function resolveNonClass($parameter){
+
+        if ($parameter->isDefaultValueAvailable()) {
+            
+            return $parameter->getDefaultValue();
+            
+        }
+        
+        throw new Exception('Error:Missing parameter.');
+
+    }
+    
+}
---
+++ app/Controller/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\Controller;
+
+class Controller{
+
+    protected static $models;
+
+    public function __construct($model){
+
+        //注册！
+        $model->Index = 'app\Model\IndexModel';
+        $model->Admin = 'app\Model\AdminModel';
+        $model->User = 'app\Model\UserModel';
+        $model->Article = 'app\Model\ArticleModel';
+        $model->Comment = 'app\Model\CommentModel';
+        $model->Diary = 'app\Model\DiaryModel';
+        $model->Tag = 'app\Model\TagModel';
+        $model->Memcache = 'AliceFrameWork\Memcache';
+        $model->WeixinClass = 'AliceFrameWork\weixin\WeixinClass';
+        $model->Weixin = 'AliceFrameWork\weixin\Weixin';
+        $model->JinShan = 'AliceFrameWork\JinShan';
+        $model->Qiniu = 'AliceFrameWork\Qiniu';
+        $model->SmtpMail = 'AliceFrameWork\SmtpMail';
+        self::$models = $model;
+
+    }
+
+}
