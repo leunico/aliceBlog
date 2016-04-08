74d0a1df1ea13515eae4c74d548cc689f0795ddb
leunico <867426952@qq.com>
2016-04-08 11:05:54 +0800

    Update UserModel.php

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -1,75 +1,59 @@
-<?php
-
-namespace app\Model;
+<?php namespace app\Model;
 
 use AliceFrameWork\Example;
 
-class UserModel{
-	
-	private static $_table = 'admin_user';
-	
-	public static function getUserList($page){
-		
-		$ret = new Example(self::$_table);
-		return $ret->Data('a.* ,COUNT(b.id) AS count')->Join('info_article','LEFT','a.id=b.uid')->where('','')->group('a.id')->order(array('count','ctime'),array('DESC','DESC'))->Paginate($page,10);				
-		
-	}
-	
-	public static function setUserBlock($id,$fields){
-		
-		$ret = new Example(self::$_table);
-		return $ret->Update($fields)->where('id',$id)->change();
-		
-	}
-	
-	public static function InsertUser($fields){
-		
-		$ret = new Example(self::$_table);
-		return $ret->Insert($fields)->change();	
-			
-	}
-	
-	public static function delUser($id){
-		
-		$ret = new Example(self::$_table);
-		return $ret->Delete()->where('id',$id)->change();
-		
-	}
-	
-	public static function getOneUser($type,$val){
-		
-		$ret = new Example(self::$_table);
-		return $ret->Find($type,$val);
-		
-	}
-	
-	public static function editUser($id,$fields){
-		
-		$ret = new Example(self::$_table);
-		return $ret->Update($fields)->where('id',$id)->change();
-		
-	}
-	
-	public static function editArticleAuthor($username,$id){
-		
-		$ret = new Example('info_article');
-		return $ret->Update(array('author'=>$username))->where('uid',$id)->change();
-						
-	}
+class UserModel
+{
+    private $example;
     
-    public static function getweixinUser($scene_id){
-        
-        $ret = new Example('weixin_user');
-		return $ret->Find('scene_id',$scene_id);
-        
+    public function __construct()
+    {
+        $this->example = new Example('admin_user');
     }
     
-    public static function delweixinUser($scene_id){
-        
-        $ret = new Example('weixin_user');
-		return $ret->Delete()->where('scene_id',$scene_id)->change();
-        
-    }	
-	
-	
+    public function getUserList($page)
+    {
+        return $this->example->Data('a.* ,COUNT(b.id) AS count')->Join('info_article', 'LEFT', 'a.id=b.uid')->where('', '')->group('a.id')->order(array('count', 'ctime'), array('DESC', 'DESC'))->Paginate($page, 10);
+    }
+    
+    public function setUserBlock($id, $fields)
+    {
+        return $this->example->Update($fields)->where('id', $id)->change();
+    }
+    
+    public function InsertUser($fields)
+    {
+        return $this->example->Insert($fields)->change();
+    }
+    
+    public function delUser($id)
+    {
+        return $this->example->Delete()->where('id', $id)->change();
+    }
+    
+    public function getOneUser($type, $val)
+    {
+        return $this->example->Find($type, $val);
+    }
+    
+    public function editUser($id, $fields)
+    {
+        return $this->example->Update($fields)->where('id', $id)->change();
+    }
+    
+    public function editArticleAuthor($username, $id)
+    {
+        return $this->example->setBind('info_article')->Update(array('author' => $username))->where('uid', $id)->change();
+    }
+    
+    public function getweixinUser($scene_id)
+    {
+        return $this->example->setBind('weixin_user')->Find('scene_id', $scene_id);
+    }
+    
+    public function delweixinUser($scene_id)
+    {
+        return $this->example->setBind('weixin_user')->Delete()->where('scene_id', $scene_id)->change();
+    }
+    
 }
